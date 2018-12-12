78f8f6b1d76741c67c267684a0a949643a010aa8
eberki <gonosztaviszorny@gmail.com>
2018-12-12 23:22:11 +0100

    Day 6 part 2 initial

--- Day6/Program.cs
+++ Day6/Program.cs
@@ -30,9 +30,15 @@ namespace Day6
 
             var coordinateTerritories = CoordinateTerritoriesInBoundingRectangle(coordinates, boundingRectangle);
 
-            var coordinateWithMaxFiniteTerritory = coordinateTerritories.OrderByDescending(kv => kv.Value).Where(kv => !coordinatesWithInfiniteSpace.Contains(kv.Key)).First();
+            var coordinateWithMaxFiniteTerritory = coordinateTerritories.Item1.OrderByDescending(kv => kv.Value).Where(kv => !coordinatesWithInfiniteSpace.Contains(kv.Key)).First();
 
-            Console.WriteLine($"Coordinate with max finite territory: ({coordinateWithMaxFiniteTerritory.Key.X},{coordinateWithMaxFiniteTerritory.Key.Y}) with territory {coordinateWithMaxFiniteTerritory.Value}");
+
+
+            Console.WriteLine($"Coordinate with max finite territory: ({coordinateWithMaxFiniteTerritory.Key.X},{coordinateWithMaxFiniteTerritory.Key.Y})" +
+                              $" with territory {coordinateWithMaxFiniteTerritory.Value}");
+
+            // Task 2
+            var safePlaces = coordinateTerritories.Item2.Where(kv => kv.Value < 10000);
 
             Console.ReadLine();
         }
@@ -101,12 +107,13 @@ namespace Day6
             return coordinatesWithInfiniteSpace;
         }
 
-        static IDictionary<Coordinate, int> CoordinateTerritoriesInBoundingRectangle(List<Coordinate> coordinates, Rectangle boundingRectangle)
+        static (IDictionary<Coordinate, int>, IDictionary<Coordinate, int>)CoordinateTerritoriesInBoundingRectangle(List<Coordinate> coordinates, Rectangle boundingRectangle)
         {
             var sw = Stopwatch.StartNew();
             var coordinatesWithInfiniteSpace = new HashSet<Coordinate>();
 
             var coordinateTerritories = new Dictionary<Coordinate, int>();
+            var locationSafeness = new Dictionary<Coordinate, int>();
 
             for (int i = 0; i < coordinates.Count; i++)
             {
@@ -119,28 +126,34 @@ namespace Day6
                 {
                     int minDistance = int.MaxValue;
                     Coordinate closestCoordinate = null;
+                    int sumDistance = 0;
                     foreach (var coordinate in coordinates)
                     {
-                        if (coordinate.DistanceTo(x, y) < minDistance)
+                        int distance = coordinate.DistanceTo(x, y);
+                        if (distance < minDistance)
                         {
-                            minDistance = coordinate.DistanceTo(x, y);
+                            minDistance = distance;
                             closestCoordinate = coordinate;
                         }
+
+                        sumDistance += distance;
                     }
 
                     coordinateTerritories[closestCoordinate]++;
+                    locationSafeness.Add(new Coordinate(x, y), sumDistance);
                 }
             }
 
             sw.Stop();
             Console.WriteLine(sw.ElapsedMilliseconds);
 
-            return coordinateTerritories;
+            return (coordinateTerritories, locationSafeness);
         }
 
         
     }
 
+    [DebuggerDisplay("X: {X}, Y: {Y}")]
     class Coordinate
     {
         public Coordinate(int x, int y)
@@ -149,9 +162,9 @@ namespace Day6
             Y = y;
         }
 
-        public int X { get; set; }
+        public int X { get; }
 
-        public int Y { get; set; }
+        public int Y { get; }
 
         public int DistanceTo(Coordinate other)
         {
@@ -161,6 +174,11 @@ namespace Day6
         public int DistanceTo(int otherX, int otherY)
         {
             return Math.Abs(otherX - X) + Math.Abs(otherY - Y);
+        }
+
+        public bool IsNeighbour(Coordinate other)
+        {
+            return Math.Abs(other.X - X) <= 1 && Math.Abs(other.Y - Y) <= 1;
         }
     }
 }
