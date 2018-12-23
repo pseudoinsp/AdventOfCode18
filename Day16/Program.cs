7eb3d546971a0467720ba5b16d7b2d27df34c64b
eberki <gonosztaviszorny@gmail.com>
2018-12-23 18:41:33 +0100

    introduced Sample class

--- Day16/Program.cs
+++ Day16/Program.cs
@@ -25,18 +25,54 @@ namespace Day16
 
             ParseInput(inputReadingTask.Result);
 
-            var applicableOPCodesOfSamples = new Dictionary<(List<int>, List<int>, List<int>), IList<IOPCode>>();
+            var applicableOPCodesOfSamples = new Dictionary<Sample, IList<Type>>();
+
+            HashSet<Type> remainingCodeIds = new HashSet<Type>()
+            {
+                typeof(Addr), typeof(Addi),
+                typeof(Mulr), typeof(Muli),
+                typeof(Banr), typeof(Bani),
+                typeof(Borr), typeof(Bori),
+                typeof(Setr), typeof(Seti),
+                typeof(Gtir), typeof(Gtri), typeof(Gtrr),
+                typeof(Eqir), typeof(Eqri), typeof(Eqrr)
+            };
+
+            IDictionary<IOPCode, int> opcodeIds = new Dictionary<IOPCode, int>();
 
             foreach (var sample in _samples)
             {
-                applicableOPCodesOfSamples.Add(sample, new List<IOPCode>());
+                applicableOPCodesOfSamples.Add(sample, new List<Type>());
 
                 foreach (var opcode in _opCodes)
                 {
-                    if (opcode.CanBehave(sample.Item1, sample.Item2, sample.Item3))
-                        applicableOPCodesOfSamples[sample].Add(opcode);
+                    if (opcode.CanBehave(sample))
+                        applicableOPCodesOfSamples[sample].Add(opcode.GetType());
+                }
+
+                //if(applicableOPCodesOfSamples[sample].Count == 1)
+                //{
+                //    IOPCode opcode = applicableOPCodesOfSamples[sample].First();
+
+                //    if(remainingCodeIds.Contains(opcode.GetType()))
+                //    {
+                //        opcodeIds.Add(applicableOPCodesOfSamples[sample].First(), sample.Item2[0]);
+                //        remainingCodeIds.Remove(opcode.GetType());
+                //    }
+                //}
+            }
+
+            IEnumerable<IGrouping<int, KeyValuePair<Sample, IList<Type>>>> samplesByOpcodes = applicableOPCodesOfSamples.GroupBy(kv => kv.Key.OpCode);
+
+            foreach (var sampleGroup in samplesByOpcodes)
+            {
+                foreach (KeyValuePair<Sample, IList<Type>> sample in sampleGroup)
+                {
+
                 }
             }
+
+            //var samplesWith1applicableOPcodes = applicableOPCodesOfSamples.Where(kv => kv.Value.Count == 1);
 
             var samplesWith3applicableOPcodes = applicableOPCodesOfSamples.Where(kv => kv.Value.Count >= 3);
 
@@ -47,19 +83,40 @@ namespace Day16
 
         static void ParseInput(string[] lines)
         {
-            for (int i = 0; i < lines.Length; i += 4)
+            for (int i = 1; i < lines.Length; i += 4)
             {
-                List<int> before = new List<int>()
+                _samples.Add(Sample.FromString(lines.Skip(i-1).Take(3).ToArray()));
+            }
+        }
+
+        static IList<IOPCode> _opCodes;
+
+        static readonly List<Sample> _samples = new List<Sample>();
+    }
+
+    class Sample
+    {
+        private Sample(List<int> registerBefore, List<int> instruction, List<int> registerAfter)
+        {
+            RegisterBefore = registerBefore;
+            RegisterAfter = registerAfter;
+            Instruction = instruction;
+            OpCode = instruction[0];
+        }
+
+        public static Sample FromString(string[] lines)
+        {
+            List<int> before = new List<int>()
                 {
-                    int.Parse(lines[i][9].ToString()),
-                    int.Parse(lines[i][12].ToString()),
-                    int.Parse(lines[i][15].ToString()),
-                    int.Parse(lines[i][18].ToString()),
+                    int.Parse(lines[0][9].ToString()),
+                    int.Parse(lines[0][12].ToString()),
+                    int.Parse(lines[0][15].ToString()),
+                    int.Parse(lines[0][18].ToString()),
                 };
 
-                var instructionParts = lines[i + 1].Split(' ');
+            var instructionParts = lines[1].Split(' ');
 
-                List<int> instructions = new List<int>()
+            List<int> instructions = new List<int>()
                 {
                     int.Parse(instructionParts[0].ToString()),
                     int.Parse(instructionParts[1].ToString()),
@@ -67,165 +124,166 @@ namespace Day16
                     int.Parse(instructionParts[3].ToString()),
                 };
 
-                List<int> after = new List<int>()
+            List<int> after = new List<int>()
                 {
-                    int.Parse(lines[i+2][9].ToString()),
-                    int.Parse(lines[i+2][12].ToString()),
-                    int.Parse(lines[i+2][15].ToString()),
-                    int.Parse(lines[i+2][18].ToString()),
+                    int.Parse(lines[2][9].ToString()),
+                    int.Parse(lines[2][12].ToString()),
+                    int.Parse(lines[2][15].ToString()),
+                    int.Parse(lines[2][18].ToString()),
                 };
 
-                _samples.Add((before, instructions, after));
-            }
+            return new Sample(before, instructions, after);
         }
 
-        static IList<IOPCode> _opCodes;
+        public List<int> RegisterBefore { get; }
+        public List<int> Instruction { get; }
+        public List<int> RegisterAfter { get; }
 
-        static readonly List<(List<int>, List<int>, List<int>)> _samples = new List<(List<int>, List<int>, List<int>)>();
+        public int OpCode { get; set; }
     }
 
     interface IOPCode
     {
-        bool CanBehave(List<int> registerBefore, List<int> instruction, List<int> registerAfter);
+        bool CanBehave(Sample sample);
     }
 
     class Addr : IOPCode
     {
-        public bool CanBehave(List<int> registerBefore, List<int> instruction, List<int> registerAfter)
+        public bool CanBehave(Sample sample)
         {
-            return registerAfter[instruction[3]] == registerBefore[instruction[1]] + registerBefore[instruction[2]];
+            return sample.RegisterAfter[sample.Instruction[3]] == sample.RegisterBefore[sample.Instruction[1]] + sample.RegisterBefore[sample.Instruction[2]];
         }
     }
 
     class Addi : IOPCode
     {
-        public bool CanBehave(List<int> registerBefore, List<int> instruction, List<int> registerAfter)
+        public bool CanBehave(Sample sample)
         {
-            return registerAfter[instruction[3]] == registerBefore[instruction[1]] + instruction[2];
+            return sample.RegisterAfter[sample.Instruction[3]] == sample.RegisterBefore[sample.Instruction[1]] + sample.Instruction[2];
         }
     }
 
     class Mulr : IOPCode
     {
-        public bool CanBehave(List<int> registerBefore, List<int> instruction, List<int> registerAfter)
+        public bool CanBehave(Sample sample)
         {
-            return registerAfter[instruction[3]] == registerBefore[instruction[1]] * registerBefore[instruction[2]];
+            return sample.RegisterAfter[sample.Instruction[3]] == sample.RegisterBefore[sample.Instruction[1]] * sample.RegisterBefore[sample.Instruction[2]];
         }
     }
 
     class Muli : IOPCode
     {
-        public bool CanBehave(List<int> registerBefore, List<int> instruction, List<int> registerAfter)
+        public bool CanBehave(Sample sample)
         {
-            return registerAfter[instruction[3]] == registerBefore[instruction[1]] * instruction[2];
+            return sample.RegisterAfter[sample.Instruction[3]] == sample.RegisterBefore[sample.Instruction[1]] * sample.Instruction[2];
         }
     }
 
     class Banr : IOPCode
     {
-        public bool CanBehave(List<int> registerBefore, List<int> instruction, List<int> registerAfter)
+        public bool CanBehave(Sample sample)
         {
-            return registerAfter[instruction[3]] == (registerBefore[instruction[1]] & registerBefore[instruction[2]]);
+            return sample.RegisterAfter[sample.Instruction[3]] == (sample.RegisterBefore[sample.Instruction[1]] & sample.RegisterBefore[sample.Instruction[2]]);
         }
     }
 
     class Bani : IOPCode
     {
-        public bool CanBehave(List<int> registerBefore, List<int> instruction, List<int> registerAfter)
+        public bool CanBehave(Sample sample)
         {
-            return registerAfter[instruction[3]] == (registerBefore[instruction[1]] & instruction[2]);
+            return sample.RegisterAfter[sample.Instruction[3]] == (sample.RegisterBefore[sample.Instruction[1]] & sample.Instruction[2]);
         }
     }
 
     class Borr : IOPCode
     {
-        public bool CanBehave(List<int> registerBefore, List<int> instruction, List<int> registerAfter)
+        public bool CanBehave(Sample sample)
         {
-            return registerAfter[instruction[3]] == (registerBefore[instruction[1]] | registerBefore[instruction[2]]);
+            return sample.RegisterAfter[sample.Instruction[3]] == (sample.RegisterBefore[sample.Instruction[1]] | sample.RegisterBefore[sample.Instruction[2]]);
         }
     }
 
     class Bori : IOPCode
     {
-        public bool CanBehave(List<int> registerBefore, List<int> instruction, List<int> registerAfter)
+        public bool CanBehave(Sample sample)
         {
-            return registerAfter[instruction[3]] == (registerBefore[instruction[1]] | instruction[2]);
+            return sample.RegisterAfter[sample.Instruction[3]] == (sample.RegisterBefore[sample.Instruction[1]] | sample.Instruction[2]);
         }
     }
 
     class Setr : IOPCode
     {
-        public bool CanBehave(List<int> registerBefore, List<int> instruction, List<int> registerAfter)
+        public bool CanBehave(Sample sample)
         {
-            return registerAfter[instruction[3]] == registerBefore[instruction[1]];
+            return sample.RegisterAfter[sample.Instruction[3]] == sample.RegisterBefore[sample.Instruction[1]];
         }
     }
 
     class Seti : IOPCode
     {
-        public bool CanBehave(List<int> registerBefore, List<int> instruction, List<int> registerAfter)
+        public bool CanBehave(Sample sample)
         {
-            return registerAfter[instruction[3]] == instruction[1];
+            return sample.RegisterAfter[sample.Instruction[3]] == sample.Instruction[1];
         }
     }
 
     class Gtir : IOPCode
     {
-        public bool CanBehave(List<int> registerBefore, List<int> instruction, List<int> registerAfter)
+        public bool CanBehave(Sample sample)
         {
-            var AgreaterThanB = instruction[1] > registerBefore[instruction[2]];
+            var AgreaterThanB = sample.Instruction[1] > sample.RegisterBefore[sample.Instruction[2]];
 
-            return (registerAfter[instruction[3]] == 1 && AgreaterThanB) || (registerAfter[instruction[3]] == 0 && !AgreaterThanB);
+            return (sample.RegisterAfter[sample.Instruction[3]] == 1 && AgreaterThanB) || (sample.RegisterAfter[sample.Instruction[3]] == 0 && !AgreaterThanB);
         }
     }
 
     class Gtri : IOPCode
     {
-        public bool CanBehave(List<int> registerBefore, List<int> instruction, List<int> registerAfter)
+        public bool CanBehave(Sample sample)
         {
-            var AgreaterThanB = registerBefore[instruction[1]] > instruction[2];
+            var AgreaterThanB = sample.RegisterBefore[sample.Instruction[1]] > sample.Instruction[2];
 
-            return (registerAfter[instruction[3]] == 1 && AgreaterThanB) || (registerAfter[instruction[3]] == 0 && !AgreaterThanB);
+            return (sample.RegisterAfter[sample.Instruction[3]] == 1 && AgreaterThanB) || (sample.RegisterAfter[sample.Instruction[3]] == 0 && !AgreaterThanB);
         }
     }
 
     class Gtrr : IOPCode
     {
-        public bool CanBehave(List<int> registerBefore, List<int> instruction, List<int> registerAfter)
+        public bool CanBehave(Sample sample)
         {
-            var AgreaterThanB = registerBefore[instruction[1]] > registerBefore[instruction[2]];
+            var AgreaterThanB = sample.RegisterBefore[sample.Instruction[1]] > sample.RegisterBefore[sample.Instruction[2]];
 
-            return (registerAfter[instruction[3]] == 1 && AgreaterThanB) || (registerAfter[instruction[3]] == 0 && !AgreaterThanB);
+            return (sample.RegisterAfter[sample.Instruction[3]] == 1 && AgreaterThanB) || (sample.RegisterAfter[sample.Instruction[3]] == 0 && !AgreaterThanB);
         }
     }
 
     class Eqir : IOPCode
     {
-        public bool CanBehave(List<int> registerBefore, List<int> instruction, List<int> registerAfter)
+        public bool CanBehave(Sample sample)
         {
-            var AEqualsB = instruction[1] == registerBefore[instruction[2]];
+            var AEqualsB = sample.Instruction[1] == sample.RegisterBefore[sample.Instruction[2]];
 
-            return (registerAfter[instruction[3]] == 1 && AEqualsB) || (registerAfter[instruction[3]] == 0 && !AEqualsB);
+            return (sample.RegisterAfter[sample.Instruction[3]] == 1 && AEqualsB) || (sample.RegisterAfter[sample.Instruction[3]] == 0 && !AEqualsB);
         }
     }
 
     class Eqri : IOPCode
     {
-        public bool CanBehave(List<int> registerBefore, List<int> instruction, List<int> registerAfter)
+        public bool CanBehave(Sample sample)
         {
-            var AEqualsB = registerBefore[instruction[1]] == instruction[2];
+            var AEqualsB = sample.RegisterBefore[sample.Instruction[1]] == sample.Instruction[2];
 
-            return (registerAfter[instruction[3]] == 1 && AEqualsB) || (registerAfter[instruction[3]] == 0 && !AEqualsB);
+            return (sample.RegisterAfter[sample.Instruction[3]] == 1 && AEqualsB) || (sample.RegisterAfter[sample.Instruction[3]] == 0 && !AEqualsB);
         }
     }
 
     class Eqrr : IOPCode
     {
-        public bool CanBehave(List<int> registerBefore, List<int> instruction, List<int> registerAfter)
+        public bool CanBehave(Sample sample)
         {
-            var AEqualsB = registerBefore[instruction[1]] == registerBefore[instruction[2]];
+            var AEqualsB = sample.RegisterBefore[sample.Instruction[1]] == sample.RegisterBefore[sample.Instruction[2]];
 
-            return (registerAfter[instruction[3]] == 1 && AEqualsB) || (registerAfter[instruction[3]] == 0 && !AEqualsB);
+            return (sample.RegisterAfter[sample.Instruction[3]] == 1 && AEqualsB) || (sample.RegisterAfter[sample.Instruction[3]] == 0 && !AEqualsB);
         }
     }
 }
